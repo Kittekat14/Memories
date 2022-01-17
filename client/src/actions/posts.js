2e6364b1eb6d423384a4c08c02d9c721051a8b43
Kittekat14 <katihofstetter@gmail.com>
2022-01-17 18:53:05 +0100

    using constants as actions separately

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,11 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
+
 import * as api from "../api";
 
 //Action creators with Redux thunk and async functions
@@ -5,7 +13,7 @@ import * as api from "../api";
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-    dispatch({ type: "FETCH_ALL", payload: data }); //no return because of redux thunk
+    dispatch({ type: FETCH_ALL, payload: data }); //no return because of redux thunk
   } catch (error) {
     console.log(error.message);
   }
@@ -14,7 +22,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -23,7 +31,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error); //always console.log error, not error.message: gives more infos
   }
@@ -32,7 +40,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id); // no const { data } = response.data, because we aren't interested in the data when we delete
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error); //always console.log error, not error.message: gives more infos
   }
@@ -41,7 +49,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id); // no const { data } = response.data, because we aren't interested in the data when we delete
-    dispatch({ type: "LIKE", payload: data });
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error); //always console.log error, not error.message: gives more infos
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,18 +1,26 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
+
 const posts = (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
+    case LIKE:
       return posts.map(
         (post) =>
           post._id === action.payload._id
             ? action.payload
             : post /*if in the action.payload is the _id, use that new payload, otherwise old post-payload */
       );
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload); // keep all posts except one inside action.payload!
     default:
       return posts;
