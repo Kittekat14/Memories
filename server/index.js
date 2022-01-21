26d208071e0d1c3733938d25db5f34354e89b1e5
Kittekat14 <katihofstetter@gmail.com>
2022-01-21 11:23:15 +0100

    try to change the PORT but doesn't work so let it be 5000

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use("/posts", postRoutes);
 
 // https://wwww.mongodb.com/cloud/atlas
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
