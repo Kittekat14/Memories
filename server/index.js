2224a591cd7b9bd4847e3393579f478bd176c984
Kittekat14 <katihofstetter@gmail.com>
2022-01-21 11:56:43 +0100

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -15,9 +15,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello to Memories API");
+});
+
 // https://wwww.mongodb.com/cloud/atlas
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // will be populated by heroku
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
